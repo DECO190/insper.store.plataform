9a67232968d95d0a19dd93667e782967064e0b76
DECO190 <deco9871@gmail.com>
2025-04-23 15:23:55 -0300

    update .gitignore and clone scripts; add exchange service to compose.yaml

--- api/clone.sh
+++ api/clone.sh
@@ -1,10 +1,12 @@
-git clone git@github.com:hsandmann/insper.store.account.git
-git clone git@github.com:hsandmann/insper.store.account-service.git
+git clone git@github.com:DECO190/insper.store.account.git
+git clone git@github.com:DECO190/insper.store.account-service.git
 
-git clone git@github.com:hsandmann/insper.store.auth.git
-git clone git@github.com:hsandmann/insper.store.auth-service.git
+git clone git@github.com:DECO190/insper.store.auth.git
+git clone git@github.com:DECO190/insper.store.auth-service.git
 
-git clone git@github.com:hsandmann/insper.store.gateway-service.git
+git clone git@github.com:DECO190/insper.store.gateway-service.git
+
+git clone git@github.com:DECO190/insper.store.exchange-service.git
 
 mv insper.store.account account
 mv insper.store.account-service account-service
@@ -13,3 +15,5 @@ mv insper.store.auth auth
 mv insper.store.auth-service auth-service
 
 mv insper.store.gateway-service gateway-service
+
+mv insper.store.exchange-service exchange-service
